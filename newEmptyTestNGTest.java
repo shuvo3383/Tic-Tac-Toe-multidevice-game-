d5f4621b26aff3effb0e892e84cf9df3bf55d28e
Shuvo das <88080595+shuvo3383@users.noreply.github.com>
2022-10-24 22:01:55 +0600

    Add files via upload

---
+++ newEmptyTestNGTest.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import static org.testng.Assert.*;
+
+/**
+ *
+ * @author Shuvo
+ */
+public class newEmptyTestNGTest {
+    
+    public newEmptyTestNGTest() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+
+    @org.testng.annotations.BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @org.testng.annotations.AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @org.testng.annotations.BeforeMethod
+    public void setUpMethod() throws Exception {
+    }
+
+    @org.testng.annotations.AfterMethod
+    public void tearDownMethod() throws Exception {
+    }
+}
